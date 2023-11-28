f436d08103530b47d7d69ead9acfb271bbcddd6e
Nothingg <nothing9537@gmail.com>
2023-11-27 22:46:13 -0500

    Computed properties - DailyAccountState class

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var accountState = new DailyAccountState(2000, 200);
+
+Console.WriteLine(accountState.EndOfDayState);
+Console.WriteLine(accountState.Report);
+
+Console.ReadKey();
+
+public class DailyAccountState
+{
+    public int InitialState { get; }
+
+    public int SumOfOperations { get; }
+
+    public DailyAccountState(int initialState, int sumOfOperations)
+    {
+        InitialState = initialState;
+        SumOfOperations = sumOfOperations;
+    }
+
+    public int EndOfDayState => InitialState + SumOfOperations;
+    public string Report => $"Day: {DateTime.Now.Day}, " +
+        $"month: {DateTime.Now.Month}, " +
+        $"year: {DateTime.Now.Year}, " +
+        $"initial state: {InitialState}, " +
+        $"end of day state: {EndOfDayState}";
+}
