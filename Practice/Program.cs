031109f4f99dade72af4cdce43498a79ddc7c5c9
Nothingg <nothing9537@gmail.com>
2023-11-29 09:58:05 -0600

    Update Program.cs

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,8 +1,46 @@
-﻿public static class StringsTransformator
+﻿public class Exercise
 {
-    public static string TransformSeparators(
-        string input,
-        string originalSeparator,
-        string targetSeparator
-        ) => string.Join(targetSeparator, input.Split(originalSeparator));
+    public List<int> GetCountsOfAnimalsLegs()
+    {
+        var animals = new List<Animal>
+            {
+                new Lion(),
+                new Tiger(),
+                new Duck(),
+                new Spider()
+            };
+
+        var result = new List<int>();
+
+        foreach (var animal in animals)
+        {
+            result.Add(animal.NumberOfLegs);
+        }
+
+        return result;
+    }
+
+    public class Animal
+    {
+        public virtual int NumberOfLegs { get; set; } = 4;
+    }
+
+    public class Lion : Animal
+    {
+
+    }
+
+    public class Tiger : Animal
+    {
+
+    }
+
+    public class Duck : Animal
+    {
+        public override int NumberOfLegs { get; set; } = 2;
+    }
+    public class Spider : Animal
+    {
+        public override int NumberOfLegs { get; set; } = 8;
+    }
 }
