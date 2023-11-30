0a1276d6c56656759bf9ddd014dda39c7901b850
Nothingg <nothing9537@gmail.com>
2023-11-30 10:02:35 -0600

    Update Program.cs

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,58 +1,23 @@
-﻿public static class ExerciseShapes
-{
-    public static List<double> GetShapesAreas(List<Shape> shapes)
-    {
-        var result = new List<double>();
+﻿var numbers = new List<int> {  };
 
-        foreach (var shape in shapes)
+foreach (var item in numbers.TakeEverySecond())
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadKey();
+
+public static class ListExtentions
+{
+    public static List<int> TakeEverySecond(this List<int> numbers)
+    {
+        var result = new List<int>();
+
+        for (int i = 0; i < numbers.Count; i += 2)
         {
-            result.Add(shape.CalculateArea());
+            result.Add(numbers[i]);
         }
 
         return result;
     }
-}
-
-public abstract class Shape
-{
-    public abstract double CalculateArea();
-}
-
-public class Square : Shape
-{
-    public double Side { get; }
-
-    public Square(double side)
-    {
-        Side = side;
-    }
-
-    public override double CalculateArea() => Side * Side;
-}
-
-
-public class Rectangle : Shape
-{
-    public double Width { get; }
-    public double Height { get; }
-
-    public Rectangle(double width, double height)
-    {
-        Width = width;
-        Height = height;
-    }
-
-    override public double CalculateArea() => Width * Height;
-}
-
-public class Circle : Shape
-{
-    public double Radius { get; }
-
-    public Circle(double radius)
-    {
-        Radius = radius;
-    }
-
-    override public double CalculateArea() => Math.PI * Math.Pow(Radius, 2);
 }
