dfd8bb6f4ea211347443bffe922636dd7de9bfd6
Nothingg <nothing9537@gmail.com>
2023-12-05 18:56:07 -0600

    Update Program.cs

--- People/Program.cs
+++ People/Program.cs
@@ -1,4 +1,31 @@
-﻿Console.ReadKey();
+﻿try
+{
+    Person invalidPersonAll = new Person(null, -100);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+try
+{
+    Person invalidPersonName = new Person("", -100);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+try
+{
+    Person invalidPersonYear = new Person("Valid name", 2024);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.ReadKey();
 
 class Person
 {
@@ -7,6 +34,21 @@ class Person
 
     public Person(string name, int yearOfBirth)
     {
+        if (name is null)
+        {
+            throw new ArgumentNullException("The name cannot be null.");
+        }
+
+        if (name == string.Empty)
+        {
+            throw new ArgumentException("The name cannot be empty.");
+        }
+
+        if (yearOfBirth < 1900 || yearOfBirth > DateTime.Now.Year)
+        {
+            throw new ArgumentOutOfRangeException("The year of birth must be bewteen 1900 and the current year.");
+        }
+
         Name = name;
         YearOfBirth = yearOfBirth;
     }
