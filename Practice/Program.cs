5613c43fe3326fb2652969c8afbe5e2966755f03
Nothingg <nothing9537@gmail.com>
2023-12-12 13:56:46 -0600

    some practice

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -4,7 +4,11 @@ numbers.AddToFront<int>(-7);
 var decimals = new List<decimal> { 1m, 2.2m, 5m, 9.6m, 8.3m };
 var ints = decimals.ConvertTo<decimal, int>();
 
+var floats = new List<float> { 1.1f, 2.5f, 3f, 3.1415f };
+var longs = floats.ConvertTo<float, long>();
+
 Tuple<int, int> minAndMax = GetMinAndMax(numbers);
+
 Console.WriteLine($"Min in a collection is: {minAndMax.Item1}");
 Console.WriteLine($"Max in a collection is: {minAndMax.Item2}");
 
@@ -49,9 +53,15 @@ static class ListExtentions
 
         foreach (var item in sourceList)
         {
-            res.Add((TTarget)item);
+            TTarget itemAfterCasting = (TTarget)Convert.ChangeType(item, typeof(TTarget));
+            res.Add(itemAfterCasting);
         }
 
         return res;
     }
+}
+
+IEnumerable<T> CreateCollectionOfRandomLenght<T>(int maxLength) where T: new()
+{
+    var length = new Random().Next(0, maxLength);
 }
--- SwapTuple/Program.cs
+++ SwapTuple/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+static Tuple<T2, T1> SwapTupleItems<T1, T2>(Tuple<T1, T2> sourceTuple)
+{
+    return new Tuple<T2, T1>(sourceTuple.Item2, sourceTuple.Item1);
+}
