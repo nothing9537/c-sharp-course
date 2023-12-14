ceffe2990e8951c0938cae1f6ebc760a3de736d1
Nothingg <nothing9537@gmail.com>
2023-12-14 08:54:11 -0600

    Lambda expressions

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -2,13 +2,10 @@
 
 int[] numbers = new int[] { 1, 2, 6, 19, 8, 7, 21 };
 
-Console.WriteLine($"Is any number in a collection larger than 10? + {IsAny<int>(numbers, IsLargwerThan10<int>)}");
-Console.WriteLine($"Is any number in a collection even? + {IsAny<int>(numbers, IsEven<int>)}");
+Console.WriteLine($"Is any number in a collection larger than 10? {IsAny<int>(numbers, (n) => n > 10)}");
+Console.WriteLine($"Is any number in a collection even? {IsAny<int>(numbers, (n) => n % 2 == 0)}");
 
 Console.ReadKey();
-
-bool IsLargwerThan10<T>(int number) where T : INumber<T> => number > 10;
-bool IsEven<T>(int number) where T : INumber<T> => number % 2 == 0;
 
 bool IsAny<T>(IEnumerable<int> numbers, Func<int, bool> Predicate) where T : INumber<T>
 {
