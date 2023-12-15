5a6976d33f3750248fc83b22ffdab45490957001
Nothingg <nothing9537@gmail.com>
2023-12-15 16:13:14 -0600

    new Assihnment: Custom Cache

---
+++ CustomCache/Program.cs
@@ -0,0 +1,78 @@
+﻿var dataDownloader =
+    new CachingDataDownloader(
+        new PrintingDataDownloader(
+            new SlowDataDownloader()));
+
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+
+Console.ReadKey();
+
+public class Cache<TKey, TData>
+{
+    private readonly Dictionary<TKey, TData> _cachedData = new()!;
+
+    public TData Get(TKey key, Func<TKey, TData> action)
+    {
+        if (!_cachedData.ContainsKey(key))
+        {
+            _cachedData[key] = action(key);
+        }
+
+        return _cachedData[key];
+    }
+}
+
+public interface IDataDownloader
+{
+    string DownloadData(string resourceId);
+}
+
+public class CachingDataDownloader : IDataDownloader
+{
+    private readonly IDataDownloader _dataDownloader;
+    private readonly Cache<string, string> _cache = new();
+
+    public CachingDataDownloader(IDataDownloader dataDownloader)
+    {
+        _dataDownloader = dataDownloader;
+    }
+
+    public string DownloadData(string resourceId)
+    {
+        return _cache.Get(resourceId, _dataDownloader.DownloadData);
+    }
+}
+
+public class PrintingDataDownloader : IDataDownloader
+{
+    private readonly IDataDownloader _dataDownloader;
+    private readonly Cache<string, string> _cache = new();
+
+    public PrintingDataDownloader(IDataDownloader dataDownloader)
+    {
+        _dataDownloader = dataDownloader;
+    }
+
+    public string DownloadData(string resourceId)
+    {
+        var data = _dataDownloader.DownloadData(resourceId);
+        Console.WriteLine("Data is ready!");
+        return data;
+    }
+}
+
+public class SlowDataDownloader : IDataDownloader
+{
+    public string DownloadData(string resourceId)
+    {
+        Thread.Sleep(1000);
+
+        return $"Some data for {resourceId}";
+    }
+}
