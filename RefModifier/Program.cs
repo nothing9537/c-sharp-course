baf3f5d32ab3c7c765164f2972efbb9977eee2bf
Nothingg <nothing9537@gmail.com>
2023-12-18 16:00:49 -0600

    Coding Exercise | FastForwardToSummer

---
+++ RefModifier/Program.cs
@@ -0,0 +1,11 @@
+﻿public static class RefModifierFastForwardToSummerExercise
+{
+    public static void FastForwardToSummer(ref DateTime timestamp)
+    {
+        var firstDayOfSummer = new DateTime(timestamp.Year, 6, 21);
+        if (timestamp < firstDayOfSummer)
+        {
+            timestamp = firstDayOfSummer;
+        }
+    }
+}
