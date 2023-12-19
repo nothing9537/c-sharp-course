00eddd1b1d82371862917458e044a392f2db70f8
Nothingg <nothing9537@gmail.com>
2023-12-19 13:41:58 -0600

    Some changes + Codinf Exercise | AllLinesFromTextFileReader

---
+++ AllLinesFromTextFileReader/Program.cs
@@ -0,0 +1,25 @@
+﻿public class AllLinesFromTextFileReader : IDisposable
+{
+    private readonly StreamReader _streamReader;
+
+    public AllLinesFromTextFileReader(string filePath)
+    {
+        _streamReader = new StreamReader(filePath);
+    }
+
+    public void Dispose()
+    {
+        _streamReader.Dispose();
+    }
+
+    public List<string> ReadAllLines()
+    {
+        var result = new List<string>();
+        while (!_streamReader.EndOfStream)
+        {
+            result.Add(_streamReader.ReadLine());
+        }
+
+        return result;
+    }
+}
---
+++ FileWriter/Program.cs
@@ -0,0 +1,64 @@
+﻿const string filePath = "file.txt";
+
+using var writer = new FileWriter(filePath);
+
+writer.Write("some text");
+writer.Write("some other text");
+
+using var reader = new SpecificLineFromTextFileReader(filePath);
+
+var third = reader.ReadLineNumber(3);
+var fourth = reader.ReadLineNumber(4);
+
+Console.WriteLine($"Third line is: {third}");
+Console.WriteLine($"Fourth line is: {fourth}");
+
+Console.ReadKey();
+public class FileWriter : IDisposable
+{
+    private readonly StreamWriter _streamWriter;
+
+    public FileWriter(string filePath)
+    {
+        _streamWriter = new StreamWriter(filePath, true);
+    }
+
+    public void Dispose()
+    {
+        _streamWriter.Dispose();
+    }
+
+    public void Write(string text)
+    {
+        _streamWriter.WriteLine(text);
+        _streamWriter.Flush();
+    }
+}
+
+public class SpecificLineFromTextFileReader : IDisposable
+{
+    private readonly StreamReader _streamReader;
+
+    public SpecificLineFromTextFileReader(string filePath)
+    {
+        _streamReader = new StreamReader(filePath);
+    }
+
+    public void Dispose()
+    {
+        _streamReader.Dispose();
+    }
+
+    public string ReadLineNumber(int lineNumber)
+    {
+        _streamReader.DiscardBufferedData();
+        _streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
+
+        for (int i = 0; i < lineNumber - 1; i++)
+        {
+            _streamReader.ReadLine();
+        }
+
+        return _streamReader.ReadLine()!;
+    }
+}
