806d0b22e7f77ad1fd07469869c54980d079d17b
Nothingg <nothing9537@gmail.com>
2023-12-20 12:19:50 -0600

    Basic Reflection

---
+++ Reflection/Program.cs
@@ -0,0 +1,26 @@
+﻿var converter = new ObjectToTextConverter();
+var convertedValue = converter.Convert(new House("123 Maple Road", 178.9, 5));
+
+Console.WriteLine(convertedValue);
+
+Console.ReadKey();
+public class ObjectToTextConverter
+{
+    public string Convert(object obj)
+    {
+        Type type = obj.GetType();
+        var properties = type.GetProperties().Where(p => p.Name != "EqualityContract");
+
+        return string.Join(", ", properties.Select(property => $"{property.Name} is {property.GetValue(obj)}"));
+    }
+}
+
+public record Pet(string Name, PetType PetType, float Weight);
+public record House(string Address, double Area, int Floors);
+
+public enum PetType
+{
+    Cat,
+    Dog,
+    Fish
+}
