f3f8d5955c02e6760ea092ddc6cced83276e6ac1
Nothingg <nothing9537@gmail.com>
2023-12-22 10:53:25 -0600

    Coding Exercise | Equals

---
+++ Equals/Program.cs
@@ -0,0 +1,12 @@
+﻿public class FullName
+{
+    public string First { get; init; }
+    public string Last { get; init; }
+
+    public override string ToString() => $"{First} {Last}";
+
+    public override bool Equals(object? obj)
+    {
+        return obj is FullName fullName && First == fullName.First && Last == fullName.Last;
+    }
+}
