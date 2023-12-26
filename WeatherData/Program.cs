5efd8348996e8b8eb507539c2dba190d67fa7b7a
Nothingg <nothing9537@gmail.com>
2023-12-26 15:39:30 -0600

    Weather Data

---
+++ WeatherData/Program.cs
@@ -0,0 +1,48 @@
+﻿var weatherData = new WeatherData(25.1m, 65);
+var weatherDataRecord = new WeatherDataRecord(25.1m, 65);
+
+Console.WriteLine(weatherData);
+Console.WriteLine(weatherDataRecord);
+
+Console.ReadKey();
+
+public record class WeatherDataRecord(decimal Temperature, int Humidity);
+
+public class WeatherData : IEquatable<WeatherData>
+{
+    public decimal Temperature { get; }
+    public int Humidity { get; }
+
+    public WeatherData(decimal temperature, int humidity)
+    {
+        Temperature = temperature;
+        Humidity = humidity;
+    }
+
+    public override string ToString() => $"Temperature: {Temperature}, Humidity: {Humidity}";
+
+    public override bool Equals(object? obj)
+    {
+        return obj is WeatherData data && Equals(data);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Temperature, Humidity);
+    }
+
+    public bool Equals(WeatherData? other)
+    {
+        return Temperature == other!.Temperature && Humidity == other!.Humidity;
+    }
+
+    public static bool operator ==(WeatherData? left, WeatherData? right)
+    {
+        return EqualityComparer<WeatherData>.Default.Equals(left, right);
+    }
+
+    public static bool operator !=(WeatherData? left, WeatherData? right)
+    {
+        return !(left == right);
+    }
+}
