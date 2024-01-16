9689b8d55ef97d22b389113fbe598b9723bfc72b
Nothingg <nothing9537@gmail.com>
2024-01-16 12:10:45 -0600

    Coding Exercise | StringBuilder_TheReverseMethod

---
+++ StringBuilder_TheReverseMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+public static class StringBuilderExercise
+{
+    public static string Reverse(string input)
+    {
+        var stringBuilder = new StringBuilder(input.Length);
+
+        for (int i = input.Length; i >= 0; i--)
+        {
+            stringBuilder.Append(input[i]);
+        }
+
+        return stringBuilder.ToString();
+    }
+}
