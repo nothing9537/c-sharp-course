91df48bea6288e57dd280d02fd338f7461bae3a0
Nothingg <nothing9537@gmail.com>
2024-01-18 09:45:55 -0600

    Coding Exercise | IsAverageEqualTo

--- FloatingPointNumbers_IsAverageEqualTo/Program.cs
+++ FloatingPointNumbers_IsAverageEqualTo/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public static class FloatingPointNumbersExercise
+{
+    public static bool IsAverageEqualTo(this IEnumerable<double> input, double valueToBeChecked, double precision = 0.00001d)
+    {
+        bool isInputContainsNonValidMembers = input.Any(doubleNum => double.IsNaN(doubleNum) || double.IsInfinity(doubleNum));
+
+        if (isInputContainsNonValidMembers)
+        {
+            throw new ArgumentException("Input list contains NaN or Infinity so it's impossible to calculate an average.");
+        }
+
+        double averageInInput = input.Average();
+
+        return Math.Abs(averageInInput - valueToBeChecked) < precision;
+    }
+}
---
+++ Gold/Program.cs
@@ -0,0 +1,56 @@
+﻿public class GoldPriceReader
+{
+    private int _currentGoldPrice;
+    private readonly EmailPriceChangeNotifier _emailPriceChangeNotifier;
+    private readonly PushPriceChangeNotifier _pushPriceChangeNotifier;
+
+    public GoldPriceReader(EmailPriceChangeNotifier emailPriceChangeNotifier, PushPriceChangeNotifier pushPriceChangeNotifier)
+    {
+        _emailPriceChangeNotifier = emailPriceChangeNotifier;
+        _pushPriceChangeNotifier = pushPriceChangeNotifier;
+    }
+
+    public void ReadCurrentPrice()
+    {
+        _currentGoldPrice = new Random().Next(20_000, 50_000);
+
+        _emailPriceChangeNotifier.Update(_currentGoldPrice);
+        _pushPriceChangeNotifier.Update(_currentGoldPrice);
+    }
+}
+
+public class EmailPriceChangeNotifier
+{
+    private readonly decimal _notificationThreshold;
+
+    public EmailPriceChangeNotifier(decimal notificationThreshold)
+    {
+        _notificationThreshold = notificationThreshold;
+    }
+
+    public void Update(decimal price)
+    {
+        if (price > _notificationThreshold)
+        {
+            Console.WriteLine($"Sending an email saying that the gold price exceeded {_notificationThreshold} and now price is: {price}\n");
+        }
+    }
+}
+
+public class PushPriceChangeNotifier
+{
+    private readonly decimal _notificationThreshold;
+
+    public PushPriceChangeNotifier(decimal notificationThreshold)
+    {
+        _notificationThreshold = notificationThreshold;
+    }
+
+    public void Update(decimal price)
+    {
+        if (price > _notificationThreshold)
+        {
+            Console.WriteLine($"Sending a push notification that the gold price exceeded {_notificationThreshold} and now price is: {price}\n");
+        }
+    }
+}
